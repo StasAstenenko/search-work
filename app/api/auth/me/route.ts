bac8efa74629d1b9aef42c10a1f95b80e50bbb55
StasAstenenko <107870729+StasAstenenko@users.noreply.github.com>
2026-03-31 10:46:39 +0300

    for vercel

--- app/api/auth/me/route.ts
+++ app/api/auth/me/route.ts
@@ -6,34 +6,28 @@ export const runtime = 'nodejs';
 export const dynamic = 'force-dynamic';
 
 export async function GET() {
-  try {
-    const supabase = await createSupabaseServer();
+  const supabase = await createSupabaseServer();
 
-    const {
-      data: { user },
-      error,
-    } = await supabase.auth.getUser();
+  const { data, error } = await supabase.auth.getUser();
 
-    if (error || !user) {
-      return NextResponse.json({ user: null }, { status: 200 });
-    }
-
-    const userFromPrisma = await prisma.user.findUnique({
-      where: {
-        supabaseUserId: user.id,
-      },
-
-      include: {
-        favorites: true,
-      },
-    });
-
-    return NextResponse.json({
-      user: userFromPrisma,
-    });
-  } catch (error) {
-    console.error(error);
-
+  if (error) {
     return NextResponse.json({ user: null }, { status: 200 });
   }
+
+  if (!data.user) {
+    return NextResponse.json({ user: null }, { status: 200 });
+  }
+
+  const user = data.user;
+
+  const userFromPrisma = await prisma.user.findUnique({
+    where: { supabaseUserId: user.id },
+    include: { favorites: true },
+  });
+
+  if (!userFromPrisma) {
+    return NextResponse.json({ error: 'User not found' }, { status: 404 });
+  }
+
+  return NextResponse.json({ user: userFromPrisma });
 }
